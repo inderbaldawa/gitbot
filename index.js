0663bf018729b78243d5e30f4627a68917334ff7
inderbaldawa <inderbaldawa@gmail.com>
2023-07-30 11:42:34 +0530

    Added new date

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+import random from 'random';
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
-
 const FILE_PATH = './data.json';
-const DATE = moment().format();
-
-
-const data = {
-  date: DATE
+const makeCommit = n => {
+  if(n===0) return simpleGit().push()
+  const x = random(0,54);
+  const y = random(0,6);
+  const DATE = moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format();
+  const data = {
+    date: DATE
+  }
+  console.log(DATE);
+  jsonfile.writeFile(FILE_PATH, data, ()=>{
+    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }, makeCommit.bind(this,--n));
+  });
 }
-
-jsonfile.writeFile(FILE_PATH, data);
-
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }).push();
+makeCommit(10);
